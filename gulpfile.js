f5ef3011ecb4a73bcb03adc2cb9b7a214fd00e4b
kexiaofu <15622316910@163.com>
2016-11-22 17:47:23 +0800

    add es6Toes5 for js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,13 @@ var gulp = require('gulp'),
     cssmin = require('gulp-minify-css'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
-    oconcat = require('gulp-concat');
+    uglify = require('gulp-uglify'),
+    oconcat = require('gulp-concat'),
+    babel = require("gulp-babel"),
+    jsSrc = 'src/public/js/**/*.js',
+    sassSrc = 'src/public/sass/**/*.scss',
+    cssDistSrc = 'dist/public/css/';
+jsDistSrc = 'dist/public/js/';
 //这个可以让express启动
 gulp.task("node", function() {
     nodemon({
@@ -18,22 +24,29 @@ gulp.task("node", function() {
         }
     })
 });
-gulp.task('server', ["node"], function() {
-    var files = ['views/**/*.html', 'views/**/*.js', 'views/**/*.jade', 'public/**/*.*'];
-    //gulp.run(["node"]);
+gulp.task('server', function() {
+    gulp.watch(sassSrc, ['sassfile'])
+    gulp.watch(jsSrc, ['jsmin'])
+        //gulp.watch('src/public/pagecss/*.css', ['cssmin'])
+});
+
+gulp.task('dev', ["node"], function() {
+    var files = ['src/views/**/*.jade', 'src/public/**/*.*'];
+    gulp.run(["node"]);
     browserSync.init(files, {
         proxy: 'http://localhost:3000',
         browser: 'chrome',
-        notify: true,
+        notify: false,
         port: 4001
     });
-    gulp.watch('public/pagesass/**/*.scss', ['sassfile'])
-        //gulp.watch('public/pagecss/*.css', ['cssmin'])
+    gulp.watch(sassSrc, ['sassfile'])
+    gulp.watch(jsSrc, ['es6Toes5'])
+        //gulp.watch('src/public/pagecss/*.css', ['cssmin'])
     gulp.watch(files).on("change", reload);
 });
 //css压缩合并
 gulp.task('cssmin', function() {
-        return gulp.src('public/pagecss/*.css').pipe(plumber({
+        return gulp.src('src/public/css/*.css').pipe(plumber({
                 errorHandler: function(err) {
                     // display the error message
                     console.log(err);
@@ -46,12 +59,12 @@ gulp.task('cssmin', function() {
                 suffix: '.min'
             })) //rename压缩后的文件名
             .pipe(cssmin()) //执行压缩
-            .pipe(gulp.dest('public/stylesheets/')); //输出文件夹
+            .pipe(gulp.dest(cssDistSrc)); //输出文件夹
     })
     //sass配置
     //pipe(cssmin())css压缩
 gulp.task('sassfile', function() {
-    return gulp.src('public/pagesass/**/*.scss').pipe(plumber({
+    return gulp.src(sassSrc).pipe(plumber({
         errorHandler: function(err) {
             // display the error message
             console.log(err);
@@ -60,5 +73,34 @@ gulp.task('sassfile', function() {
         }
     })).pipe(sass()).pipe(cssmin()).pipe(rename({
         suffix: '.min'
-    })).pipe(gulp.dest('public/stylesheets/'));
+    })).pipe(gulp.dest(cssDistSrc));
 });
+gulp.task('jsmin', function() {
+    return gulp.src(jsSrc).pipe(babel({
+        presets: ['es2015']
+    })).pipe(plumber({
+        errorHandler: function(err) {
+            // display the error message
+            console.log(err);
+            // end the errored task
+            this.emit('end')
+        }
+    })).pipe(uglify()).pipe(rename({
+        suffix: '.min'
+    })).pipe(gulp.dest(jsDistSrc))
+})
+
+gulp.task('es6Toes5',function(){
+    return gulp.src(jsSrc).pipe(babel({
+        presets: ['es2015']
+    })).pipe(plumber({
+        errorHandler: function(err) {
+            // display the error message
+            console.log(err);
+            // end the errored task
+            this.emit('end')
+        }
+    })).pipe(rename({
+        suffix: '.min'
+    })).pipe(gulp.dest(jsDistSrc))
+})
